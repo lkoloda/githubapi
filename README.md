0371c4083f982444da202942869e7dd89d7f7857
lkoloda <leonid.koloda@gmail.com>
2019-07-20 23:20:12 +0300

    Update

--- README.md
+++
@@ -1,2 +0,0 @@
-# githubapi
-Test Task
--- src/main/java/com/testTask/gitapp/domain/dto/mapper/IMapper.java
+++
@@ -1,9 +0,0 @@
-package com.testTask.gitapp.domain.dto.mapper;
-
-import com.testTask.gitapp.domain.dto.AbstractDto;
-import com.testTask.gitapp.domain.entity.AbstractEntity;
-
-public interface IMapper<E extends AbstractEntity, D extends AbstractDto> {
-    E toEntity(D dto);
-    D toDto(E entity);
-}
